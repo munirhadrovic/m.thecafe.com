8654a4b4cffff2b36fe8c80ed0735936c7e30166
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-22 14:25:53 +0200

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Bistro 821, 821 5th Ave S, Naples, FL 34102, United States"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2394306555;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
